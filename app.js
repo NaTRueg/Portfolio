3f9ccfcaca0659e4f3a9c3be78a84eeab162411d
NaTRueg <nathan.g2710@gmail.com>
2023-01-09 19:48:31 +0100

    09/10

--- app.js
+++ app.js
@@ -352,3 +352,19 @@ class CardCarousel extends DraggingEvent {
 
 const carousel = new CardCarousel(cardsContainer)
 
+var req = new XMLHttpRequest();
+var url = "https://api.nasa.gov/planetary/apod?api_key=";
+var api_key = "KAWRehL2pZsASCBznFwsjuONu3gi29ccmduSSyN8";
+
+req.open("GET", url + api_key);
+req.send();
+
+req.addEventListener("load", function(){
+	if(req.status == 200 && req.readyState == 4){
+  	var response = JSON.parse(req.responseText);
+    document.getElementById("title").textContent = response.title;
+    document.getElementById("date").textContent = response.date;
+    document.getElementById("pic").src = response.hdurl;
+    document.getElementById("explanation").textContent = response.explanation;
+  }
+})
